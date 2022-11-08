e8924bf60d9dd9c76c0c1991f6326e268ad066b1
YOGA ADITYA MAHARDIKA <101709560+YOGAADITYAMAHARDIKA196@users.noreply.github.com>
2022-11-08 10:02:43 +0700

    Update Main.java

--- uts_2100018196_Yoga_Aditya_M/src/Main.java
+++ uts_2100018196_Yoga_Aditya_M/src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int pilih;
+        
+        Scanner in = new Scanner(System.in);
    
         System.out.println("==================================");
         System.out.println("1.Lihat Gaji Dosen");
